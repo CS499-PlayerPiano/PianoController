78cfea085648e306bfbcf683dffe4e20538bd6f0
Eric Golde <eric@golde.org>
2023-12-04 13:10:52 -0800

    Added the files for PluginPianoEmulatorGui

---
+++ src/plugins/PluginPianoEmulatorGui/PluginGui.java
@@ -0,0 +1,59 @@
+package plugins.PluginPianoEmulatorGui;
+
+import java.util.List;
+import java.util.Map;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+import plu.capstone.playerpiano.sheetmusic.Note;
+import plu.capstone.playerpiano.controller.plugin.PluginStateKeeper;
+import plugins.PluginPianoEmulatorGui.component.ComponentPiano;
+
+/**
+ * Plugin to visualize the piano keys.
+ */
+public class PluginGui extends PluginStateKeeper {
+
+    private final JFrame frame = new JFrame("Piano");
+    private final ComponentPiano piano = new ComponentPiano(this);
+
+    public enum ColorMode {
+        RAINBOW_GRADIENT,
+        TRACK_NUMBER,
+    }
+
+    @Override
+    public void onEnable() {
+
+        frame.add(new JScrollPane(piano));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+
+        SwingUtilities.invokeLater(() -> {
+            frame.setVisible(true);
+        });
+    }
+
+    @Override
+    public void setDefaultConfigValues() {
+        config.setEnum("colorMode", ColorMode.RAINBOW_GRADIENT);
+    }
+
+    @Override
+    public void onNoteChange(Note[] keys, long timestamp) {
+        for(Note note : keys) {
+            piano.setKeyLit(note);
+        }
+    }
+
+    @Override
+    public void onSongStarted(long timestamp, Map<Long, List<Note>> entireNoteMap) {
+        piano.clearLitKeys();
+    }
+
+    @Override
+    public void onSongFinished(long timestamp) {
+        piano.clearLitKeys();
+    }
+}
---
+++ src/plugins/PluginPianoEmulatorGui/component/KeyShape.java
@@ -0,0 +1,15 @@
+package plugins.PluginPianoEmulatorGui.component;
+
+import java.awt.Shape;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Represents a key shape and whether it is a black or white key
+ */
+@Getter
+@AllArgsConstructor
+public class KeyShape {
+    private final Shape shape;
+    private final boolean black;
+}

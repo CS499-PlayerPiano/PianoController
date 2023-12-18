085d2f9364ebf0177ccb923ca9e05140c93287ed
Eric Golde <eric@golde.org>
2023-12-18 13:14:40 -0800

    Refactoring packages names

--- src/plu/capstone/playerpiano/controller/plugin/PluginInstances.java
+++ src/plu/capstone/playerpiano/controller/plugin/PluginInstances.java
@@ -1,22 +1,19 @@
 package plu.capstone.playerpiano.controller.plugin;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import plu.capstone.playerpiano.controller.plugins.PluginArduinoPiano.PluginRealPiano;
 import plu.capstone.playerpiano.controller.plugins.PluginLogger.PluginLogger;
 import plu.capstone.playerpiano.controller.plugins.PluginMidiKeyboard.PluginMidiKeyboard;
-import plu.capstone.playerpiano.controller.plugins.PluginPianoEmulatorGui.PluginGui;
+import plu.capstone.playerpiano.controller.plugins.PluginPianoEmulatorGui.PluginVirtualPianoEmulatorGui;
 import plu.capstone.playerpiano.controller.plugins.PluginSynth.PluginSynth;
 import plu.capstone.playerpiano.controller.plugins.PluginWebAPI.PluginWebAPI;
-import plu.capstone.playerpiano.controller.plugins.test.PluginConfigTester;
-import plu.capstone.playerpiano.controller.plugins.test.TestPlugin;
 
 @AllArgsConstructor
 public enum PluginInstances {
 
     PHYSICAL_PIANO(PluginRealPiano.class),
     LOGGER(PluginLogger .class),
-    EMULATOR(PluginGui.class),
+    EMULATOR(PluginVirtualPianoEmulatorGui.class),
     SYNTH(PluginSynth.class),
     WEB_SERVER(PluginWebAPI.class),
     MIDI_KEYBOARD(PluginMidiKeyboard.class),

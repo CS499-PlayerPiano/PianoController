a924180df6284403cace86373fa75cf6aab0ba03
Eric Golde <eric@golde.org>
2024-02-27 23:01:21 -0800

    Don't think this did anything but cleans up the code a bit

--- src/plu/capstone/playerpiano/sheetmusic/MidiSheetMusic.java
+++ src/plu/capstone/playerpiano/sheetmusic/MidiSheetMusic.java
@@ -25,7 +25,7 @@ public class MidiSheetMusic extends SheetMusic {
 
     private static final Logger logger = new Logger(MidiSheetMusic.class);
     static {
-        logger.setDebugEnabled(false);
+        logger.setDebugEnabled(true);
     }
 
     /**
@@ -45,79 +45,16 @@ public class MidiSheetMusic extends SheetMusic {
         }
 
         Sequence sequence = MidiSystem.getSequence(midiFile);
+        final int resolution = sequence.getResolution();
 
         // Get the length of the song in milliseconds
         songLengthMS = sequence.getMicrosecondLength() / 1000;
 
-        //Default tempo is 500,000 microseconds per quarter note, or 120 BPM
-        long us_per_quarter = 500_000;
 
-        for (int trackNum = 0; trackNum < sequence.getTracks().length; trackNum++) {
-            Track track = sequence.getTracks()[trackNum];
-            for (int i = 0; i < track.size(); i++) {
-                MidiEvent event = track.get(i);
-                MidiMessage message = event.getMessage();
 
-                if (message instanceof MetaMessage) {
-                    MetaMessage mm = (MetaMessage) message;
-                    if (mm.getType() == MetaMessages.SET_TEMPO) {
-
-                        //Get the tempo from the meta message, so we can calculate the time of each note
-                        byte[] data = mm.getData();
-                        int tempo = ((data[0] & 0xFF) << 16) | ((data[1] & 0xFF) << 8) | (data[2] & 0xFF);
-                        us_per_quarter = tempo;
-
-                        // Calculate the time in milliseconds of the note
-                        long tick = event.getTick();
-                        long ticks_per_quarter = sequence.getResolution();
-                        long us_per_tick = us_per_quarter / ticks_per_quarter;
-                        long where = tick * us_per_tick;
-                        long whereMS = where / 1000;
-
-                        logger.debug("Tempo change: " + tempo);
-                        putEvent(whereMS, new TempoChangeEvent(tempo));
-                    }
-                }
-
-                if (message instanceof ShortMessage) {
-                    ShortMessage sm = (ShortMessage) message;
-
-                    // Calculate the time in milliseconds of the event
-                    long tick = event.getTick();
-                    long ticks_per_quarter = sequence.getResolution();
-                    long us_per_tick = us_per_quarter / ticks_per_quarter;
-                    long where = tick * us_per_tick;
-                    long whereMS = where / 1000;
-
-                    if(sm.getCommand() == ShortMessage.NOTE_ON || sm.getCommand() == ShortMessage.NOTE_OFF) {
-
-                        Note note = Note.fromMidiMessage(sm);
-
-                        if(note.isNoteOn() && note.getVelocity() == 0) {
-                            logger.warning("Note on with velocity 0 at " + whereMS + "ms");
-                        }
-                        putEvent(whereMS, note);
-                    }
-
-                    else if(sm.getCommand() == ShortMessage.CONTROL_CHANGE) {
-                        if(sm.getData1() == ControlMessages.DAMPER_PEDAL) {
-                            boolean on = sm.getData2() >= 64; //0-63 is off, 64-127 is on
-                            logger.debug("Sustain pedal: " + on);
-                            putEvent(whereMS, new SustainPedalEvent(on));
-                        }
-                    }
-
-                    //Test to see if the midi file has any control changes for the pedals.
-                    //Most midi songs seem to fake the pedal by just holding the notes for a long time.
-                    else if(sm.getCommand() == ShortMessage.CONTROL_CHANGE) {
-                        int controller = sm.getData1();
-                        int value = sm.getData2();
-                        String name = ControlMessages.getControlName(controller);
-                        logger.debug("Control change: " + ConsoleColors.PURPLE_BRIGHT + name + ConsoleColors.RESET + " value: " + ConsoleColors.PURPLE_BRIGHT + value + ConsoleColors.RESET);
-                    }
-
-                }
-            }
+        for(int i = 0; i < sequence.getTracks().length; i++) {
+            logger.info("Parsing track " + i);
+            parseTrack(resolution, sequence.getTracks()[i]);
         }
 
         logger.debug("Finished parsing midi file");
@@ -133,6 +70,77 @@ public class MidiSheetMusic extends SheetMusic {
         }
         logger.debug("Total Notes: " + amountOfNotesTotal);
         logger.debug("Max Notes at one time: " + maxForTime);
+    }
+
+    private void parseTrack(int resolution, Track track) {
+
+        //Default tempo is 500,000 microseconds per quarter note, or 120 BPM
+        long us_per_quarter = 500_000;
+
+        for (int i = 0; i < track.size(); i++) {
+            MidiEvent event = track.get(i);
+            MidiMessage message = event.getMessage();
+
+            if (message instanceof MetaMessage) {
+                MetaMessage mm = (MetaMessage) message;
+                if (mm.getType() == MetaMessages.SET_TEMPO) {
+
+                    //Get the tempo from the meta message, so we can calculate the time of each note
+                    byte[] data = mm.getData();
+                    int tempo = ((data[0] & 0xFF) << 16) | ((data[1] & 0xFF) << 8) | (data[2] & 0xFF);
+                    us_per_quarter = tempo;
+
+                    // Calculate the time in milliseconds of the note
+                    long tick = event.getTick();
+                    long ticks_per_quarter = resolution;
+                    long us_per_tick = us_per_quarter / ticks_per_quarter;
+                    long where = tick * us_per_tick;
+                    long whereMS = where / 1000;
+
+                    logger.debug("Tempo change: " + tempo);
+                    putEvent(whereMS, new TempoChangeEvent(tempo));
+                }
+            }
+
+            if (message instanceof ShortMessage) {
+                ShortMessage sm = (ShortMessage) message;
+
+                // Calculate the time in milliseconds of the event
+                long tick = event.getTick();
+                long ticks_per_quarter = resolution;
+                long us_per_tick = us_per_quarter / ticks_per_quarter;
+                long where = tick * us_per_tick;
+                long whereMS = where / 1000;
+
+                if(sm.getCommand() == ShortMessage.NOTE_ON || sm.getCommand() == ShortMessage.NOTE_OFF) {
+
+                    Note note = Note.fromMidiMessage(sm);
+
+                    if(note.isNoteOn() && note.getVelocity() == 0) {
+                        logger.warning("Note on with velocity 0 at " + whereMS + "ms");
+                    }
+                    putEvent(whereMS, note);
+                }
+
+                else if(sm.getCommand() == ShortMessage.CONTROL_CHANGE) {
+                    if(sm.getData1() == ControlMessages.DAMPER_PEDAL) {
+                        boolean on = sm.getData2() >= 64; //0-63 is off, 64-127 is on
+                        //logger.debug("Sustain pedal: " + on);
+                        putEvent(whereMS, new SustainPedalEvent(on));
+                    }
+                }
+
+                //Test to see if the midi file has any control changes for the pedals.
+                //Most midi songs seem to fake the pedal by just holding the notes for a long time.
+                else if(sm.getCommand() == ShortMessage.CONTROL_CHANGE) {
+                    int controller = sm.getData1();
+                    int value = sm.getData2();
+                    String name = ControlMessages.getControlName(controller);
+                    logger.debug("Control change: " + ConsoleColors.PURPLE_BRIGHT + name + ConsoleColors.RESET + " value: " + ConsoleColors.PURPLE_BRIGHT + value + ConsoleColors.RESET);
+                }
+
+            }
+        }
     }
 
 }

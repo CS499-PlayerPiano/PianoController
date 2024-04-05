96cc793010bdf94e8ccb374ca24fee77fca19edb
Eric Golde <eric@golde.org>
2024-04-05 10:01:16 -0700

    Moving to JSON -- doesn't compile yet

--- src/plu/capstone/playerpiano/outputs/statisticsdb/OutputStatisticsDB.java
+++ src/plu/capstone/playerpiano/outputs/statisticsdb/OutputStatisticsDB.java
@@ -1,13 +1,10 @@
 package plu.capstone.playerpiano.outputs.statisticsdb;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
+import lombok.Getter;
 import plu.capstone.playerpiano.logger.Logger;
 import plu.capstone.playerpiano.outputs.Output;
 import plu.capstone.playerpiano.sheetmusic.events.NoteEvent;
@@ -18,11 +15,10 @@ public class OutputStatisticsDB extends Output {
 
     //TODO: Make settings file
     private static final long SAVE_INTERVAL = 1000 * 60 * 1; // 1 MINUTE
-    private static final String JDBC_URL = "jdbc:sqlite:res/statistics.db";
     private static final int QUERY_TIMEOUT = 30; //seconds
     private static final int SEND_TO_WEBSITE_INTERVAL = 100; //milliseconds
 
-    private Statement statement;
+    @Getter
     private RawStatistics statistics = new RawStatistics();
 
     private Logger logger = new Logger(this);
@@ -33,32 +29,16 @@ public class OutputStatisticsDB extends Output {
     @Override
     protected void onEnable() {
 
-        try {
-            connectToDB();
-            statistics.read(statement);
-        }
-        catch(SQLException e) {
-            logger.error("Error connecting to the database", e);
-            return;
-        }
+        //Load the statistics from the DB
+        statistics.read();
 
         //Save the statistics to the DB every SAVE_INTERVAL
         new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                try {
-                    statistics.write(statement);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+                statistics.write();
             }
         }, SAVE_INTERVAL, SAVE_INTERVAL);
-    }
-
-    private void connectToDB() throws SQLException {
-        Connection connection = DriverManager.getConnection(JDBC_URL);
-        statement = connection.createStatement();
-        statement.setQueryTimeout(QUERY_TIMEOUT);
     }
 
     @Override
--- src/plu/capstone/playerpiano/outputs/statisticsdb/RawStatistics.java
+++ src/plu/capstone/playerpiano/outputs/statisticsdb/RawStatistics.java
@@ -1,32 +1,87 @@
 package plu.capstone.playerpiano.outputs.statisticsdb;
 
+import com.google.gson.JsonObject;
+import io.swagger.util.Json;
+import java.io.File;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.plaf.nimbus.State;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import plu.capstone.playerpiano.JsonConfigWrapper;
 import plu.capstone.playerpiano.sheetmusic.SheetMusicCallback;
 
 @NoArgsConstructor
-@Getter
 public class RawStatistics {
+
+    private final JsonConfigWrapper CONFIG = new JsonConfigWrapper(new File("res/statistics.json"));
+    private static final String KEY_MILLISECONDS_PLAYED = "millisecondsPlayed";
+    private static final String KEY_TOTAL_SONGS_PLAYED = "totalSongsPlayed";
+    private static final String KEY_TOTAL_NOTES_PLAYED = "totalNotesPlayed";
+    private static final String KEY_TOTAL_SUSTAIN_PEDAL_PRESSED = "totalSustainPedalPressed";
+    private static final String KEY_SONGS_PLAYED = "songsPlayed";
+
+    //TODO: Config
+    private static final int MAX_SONGS_PLAYED = 10;
 
     private long millisecondsPlayed;
     private long totalSongsPlayed;
     private long totalNotesPlayed;
     private long totalSustainPedalPressed;
 
+    private Map<String, Integer> songsPlayed = new HashMap<>();
+
     public void incrementTotalNotesPlayed() {totalNotesPlayed ++;}
-    public void incrementTotalSongsPlayed() {totalSongsPlayed ++;}
+    private void incrementTotalSongsPlayed() {totalSongsPlayed ++;}
     public void incrementTotalSustainPedalPressed() {totalSustainPedalPressed ++;}
     public void incrementMillisecondsPlayed(long milliseconds) {millisecondsPlayed += milliseconds;}
 
-    public void read(Statement statement) throws SQLException {
-
+    public void onSongPlayed(String songName) {
+        incrementTotalSongsPlayed();
+        songsPlayed.put(songName, songsPlayed.getOrDefault(songName, 0) + 1);
     }
 
-    public void write(Statement statement) throws SQLException {
+    public void read() {
+        CONFIG.loadConfig();
+        millisecondsPlayed = CONFIG.getLong(KEY_MILLISECONDS_PLAYED);
+        totalSongsPlayed = CONFIG.getLong(KEY_TOTAL_SONGS_PLAYED);
+        totalNotesPlayed = CONFIG.getLong(KEY_TOTAL_NOTES_PLAYED);
+        totalSustainPedalPressed = CONFIG.getLong(KEY_TOTAL_SUSTAIN_PEDAL_PRESSED);
 
+        songsPlayed = CONFIG.getMap(KEY_SONGS_PLAYED, String.class, Integer.class);
+    }
+
+    private void writeToConfig() {
+        CONFIG.setLong(KEY_MILLISECONDS_PLAYED, millisecondsPlayed);
+        CONFIG.setLong(KEY_TOTAL_SONGS_PLAYED, totalSongsPlayed);
+        CONFIG.setLong(KEY_TOTAL_NOTES_PLAYED, totalNotesPlayed);
+        CONFIG.setLong(KEY_TOTAL_SUSTAIN_PEDAL_PRESSED, totalSustainPedalPressed);
+        CONFIG.setMap(KEY_SONGS_PLAYED, songsPlayed); //We want to store all the songs played just incase we want to do something with them later
+    }
+
+    public void write() {
+        writeToConfig();
+        CONFIG.saveConfig(); //save to file
+    }
+
+    //Write to the config file, and return the json object
+    public JsonObject toJson() {
+        writeToConfig();
+        //Copy the object so that the original is not modified
+        final JsonObject json = CONFIG.toJsonObject().deepCopy();
+
+        JsonObject songsPlayedJson = new JsonObject();
+        //add the top 10 songs played
+        songsPlayed.entrySet().stream()
+                .sorted((a, b) -> b.getValue().compareTo(a.getValue()))
+                .limit(MAX_SONGS_PLAYED)
+                .forEach(entry -> songsPlayedJson.addProperty(entry.getKey(), entry.getValue()));
+
+        json.add(KEY_SONGS_PLAYED, songsPlayedJson);
+
+        return CONFIG.toJsonObject();
     }
 
 }
